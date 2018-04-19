c2dd36cf247037cc904d76dd9e87b8a1cac48a1a
K Rahul <rahulkadavl@gmail.com>
2018-04-19 21:40:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo chmod 755 convy.py
-sudo chmod 755 convy.sh
+sudo chmod 4755 convy.py
+sudo chmod 4755 convy.sh
 sudo cp convy.py /usr/local
 sudo cp convy.sh /usr/local
 sudo ln -s /usr/local/convy.sh /usr/local/bin/convy

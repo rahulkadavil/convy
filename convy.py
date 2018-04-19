2e7067c11d3f0c304c114064b371729f4de999a1
K Rahul <rahulkadavl@gmail.com>
2018-04-19 23:24:14 +0530

    Update convy.py

--- convy.py
+++ convy.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python
 print "Enter the value:"
 s =raw_input()
+listhex = []
+listdec = []
+listbin = []
 for i in s:
     print "Converter for " +i
     hexa = hex(ord(i))
+    listhex.append(hexa)
+    listhex.append(":")
     print "Hexadecimal :" + hexa
     inte = int(hexa[2:],16)
+    listdec.append(str(inte))
+    listdec.append(":")
     print "Decimal :" + str(inte)
     binary = bin(inte)
+    listbin.append(binary)
+    listbin.append(":")
     print "Binary :" + binary
-    print " "
-    
+print "Final hexadecimal- "+"".join(listhex)
+print "Final decimal- "+"".join(listdec)
+print "Final binary- "+"".join(listbin)
